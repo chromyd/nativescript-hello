c6e23b18ce574020ef9634201482e2b7b1fcc956
Dusan Chromy <dusan.chromy@yahoo.de>
2018-03-27 01:22:44 +0200

    Initial chore (again)

---
+++ platforms/ios/build/HelloWorld.build/Debug-iphonesimulator/HelloWorld.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/dusan/ws/HelloWorld/platforms/ios/HelloWorld/HelloWorld-Prefix.pch \
+  /Users/dusan/ws/HelloWorld/platforms/ios/internal/main.m \
+  /Users/dusan/ws/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/dusan/ws/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/dusan/ws/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/dusan/ws/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/dusan/ws/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/dusan/ws/HelloWorld/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/dusan/ws/HelloWorld/platforms/ios/internal/TNSDebugging.h \
+  /Users/dusan/ws/HelloWorld/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
